1eb10c3c4e9ab085c938acc2550b11845d9d272d
ngalela01 <149369383+ngalela01@users.noreply.github.com>
2026-06-23 17:04:55 +0200

    Init de choose-topic

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
@@ -11,7 +12,7 @@ export default function App() {
 
       {/* Subheading */}
       <Text className="text-xl dark:text-white text-gray-700 mb-8 text-center leading-relaxed">
-        Build beautiful apps with{' '}
+        Build beautiful apps with{" "}
         <Text className="text-blue-500 font-semibold">
           Expo (Router) + Uniwind 🔥
         </Text>
@@ -19,11 +20,17 @@ export default function App() {
 
       {/* Instruction text */}
       <Text className="text-base text-gray-600 dark:text-white text-center max-w-sm">
-        Start customizing your app by editing{' '}
+        Start customizing your app by editing{" "}
         <Text className="font-semibold text-gray-800 dark:text-white">
           app/index.tsx
         </Text>
       </Text>
+
+      {/* lien pour tester la page choose-topic */}
+      <Link href="/choose-topic" className="mt-6 px-4 py-2 bg-blue-500 rounded-lg">
+        <Text className="text-white font-semibold">Choose Topic</Text>
+      </Link>
+
 
       <StatusBar style="dark" />
     </View>
