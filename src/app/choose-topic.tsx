e20c38f846e5ec498bb72dbccbe7604c9ac848c0
ngalela01 <149369383+ngalela01@users.noreply.github.com>
2026-06-24 10:45:05 +0200

    Refactorisation Topic avec FlatList

--- src/app/choose-topic.tsx
+++ src/app/choose-topic.tsx
@@ -1,31 +1,34 @@
-import { ScrollView, Text, View } from "react-native";
+import { FlatList, Text, View } from "react-native";
 
 import {onBoardingTopics} from "../constants/onBoardingsTopics";
 import { TopicCard } from "../components/TopicCard";
 
 export default function ChooseTopic() {
   return (
-    <ScrollView
-      className="flex-1 bg-white px-[24px] pt-[74px]"
-    >
+    <View className="flex-1 bg-white px-[24px] pt-[74px]">
       <Text className="text-[28px] font-bold text-[#3F414E]">What Brings you</Text>
       <Text className="text-[28px] text-[#3F414E] mb-3">to Silent Moon?</Text>
       <Text className="text-[#A1A4B2] mb-6">
         choose a topic to focus on:
       </Text>
 
-      <View className="flex-row flex-wrap justify-between pb-[40px]">
-        {onBoardingTopics.map((topic) => (
+      <FlatList
+        data={onBoardingTopics}
+        keyExtractor={(topic) => topic.id}
+        numColumns={2}
+        columnWrapperClassName="justify-between"
+        contentContainerClassName="pb-[40px]"
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) => (
           <TopicCard
-            key={topic.id}
-            title={topic.title}
-            bgColor={topic.bgColor}
-            height={topic.height}
-            textColor={topic.textColor}
-            image={topic.image}
+            title={item.title}
+            bgColor={item.bgColor}
+            height={item.height}
+            textColor={item.textColor}
+            image={item.image}
           />
-        ))}
-      </View>
-    </ScrollView>
+        )}
+      />
+    </View>
   );
 }
